93ad4c63324e532517d4ca3111244a596a559ab8
ed-dickinson <edwardejd@gmail.com>
2021-10-08 15:28:27 +0100

    cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var runRouter = require('./routes/run');
 var shoeRouter = require('./routes/shoes');
 
+var cors = require('cors');
 var app = express();
 
 require('dotenv').config()
@@ -30,6 +31,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
